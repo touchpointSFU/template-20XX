8f882adb62617a2cb055517be0a14b504d5b47b1
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2025-12-11 20:09:38 -0800

    feat: localFont
    starting basic with univers--will attempt to find more weights/styles later

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const univers = localFont({
+  src: [
+    {
+      path: "../fonts/Univers-55.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Univers-55.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-univers",
+  display: "swap",
+});
+
+const univers_cond = localFont({
+  src: [
+    {
+      path: "../fonts/Univers-57.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Univers-57.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-universCondensed",
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${univers.variable} ${univers_cond.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
